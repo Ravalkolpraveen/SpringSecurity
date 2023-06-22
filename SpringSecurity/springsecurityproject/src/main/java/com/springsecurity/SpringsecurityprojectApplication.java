2b06e16b319ad857a6920fe1c87c59ab2742e4c7
Ravalkolpraveen <120642207+Ravalkolpraveen@users.noreply.github.com>
2023-06-22 16:30:01 +0530

    Login/registration
    login and registration  pages with spring security

---
+++ SpringSecurity/springsecurityproject/src/main/java/com/springsecurity/SpringsecurityprojectApplication.java
@@ -0,0 +1,15 @@
+package com.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityprojectApplication.class, args);
+		
+		
+	}
+
+}
